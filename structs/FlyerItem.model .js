55d23c58575d3091f44da99ebe752df995e24d42
Aiyush <jain.aiyush@fitster.ca>
2020-06-11 22:50:45 -0400

    Created FlyerItem class

---
+++ structs/FlyerItem.model .js
@@ -0,0 +1,30 @@
+class FlyerItem {
+    constructor(name, id, store, link) {
+        this.name = name; // string
+        this.id = id; // number
+        this.store = store; // string
+        this.link = link; // string
+
+        this.startDate = admin.firestore.Timestamp.fromDate(new Date(2020, 4, 14, 0, 0, 0));
+        this.endDate = admin.firestore.Timestamp.fromDate(new Date(2020, 4, 20, 23, 59, 59));
+    }
+
+    setPrice(price) {
+        this.price = price; // number
+    }
+
+    setServing(serving) {
+        this.serving = serving; // string
+    }
+
+    setStartDate(date){
+        this.startDate = date; // date
+    }
+
+    setEndDate(date) {
+        this.endDate = date; // date
+    }
+}
+
+
+module.exports = FlyerItem;
